1c12535e42b56c781b785814fe44995005b4a0f6
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-01-15 09:02:50 -0500

    deliverable

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('about/', views.about, name="about"),
+    path('dogs/', views.dogs_index, name="dogs_index")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+# main_app/views.py
+...
+class Dog:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+dogs = [
+    Dog('Chonners', 'tabby', 'foul little demon', 3),
+    Dog('Talo', 'tortoise shell', 'diluted tortoise shell', 2),
+    Dog('Humphrey', 'black tripod', '3 legged dog', 4)
+]
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -7,3 +21,6 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
+def dogs_index(request):
+    # grab list from line 11
+    return render(request, 'dogs/index.html', {'dogs': dogs})
